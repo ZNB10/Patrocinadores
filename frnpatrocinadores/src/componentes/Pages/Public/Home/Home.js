dbe893b4626226874a0c59968c7f02191f15df97
ZNB10 <yonathancruz2015@gmail.com>
2020-03-20 18:18:38 -0600

    Home.js Login.css

--- frnpatrocinadores/src/componentes/Pages/Public/Home/Home.js
+++ frnpatrocinadores/src/componentes/Pages/Public/Home/Home.js
@@ -3,8 +3,15 @@ import Page from '../../Page';
 
 export default ()=>{
   return (
-    <Page pageTitle="Home Page">
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, quia vel? Iure eaque ipsam, magni animi debitis sapiente consequuntur quos repudiandae. Consequatur perferendis accusantium voluptatum harum, aliquid dolor repellendus facere.</p>
+    <Page pageTitle="Patrocinadores Honduras">
+    <body>
+    <div class="col-md-12">
+      <div class="offset-md-1 pt60">
+        <h4>Haz la facturación y contabilidad de tu negocio de una forma fácil y rápida</h4>
+        <p>No pierdas tu tiempo facturando a mano, llevando inventarios y CxC en Excel, Word o sistemas complicados.</p>
+      </div>
+    </div>
+    </body>  
     </Page>
   )
 }
