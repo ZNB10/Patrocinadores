131c831757db4f382051f1fcb17f09b49defe123
Yonathan Cruz <yonathancruz2015@gmail.com>
2020-03-22 16:20:59 -0600

    Cambios Footer.css Home.js Login.css

--- frnpatrocinadores/src/componentes/Pages/Public/Home/Home.js
+++ frnpatrocinadores/src/componentes/Pages/Public/Home/Home.js
@@ -5,11 +5,9 @@ export default ()=>{
   return (
     <Page pageTitle="Patrocinadores Honduras">
     <body>
-    <div class="col-md-12">
-      <div class="offset-md-1 pt60">
-        <h4>Haz la facturación y contabilidad de tu negocio de una forma fácil y rápida</h4>
-        <p>No pierdas tu tiempo facturando a mano, llevando inventarios y CxC en Excel, Word o sistemas complicados.</p>
-      </div>
+    <div>
+      <h4>Haz la facturación y contabilidad de tu negocio de una forma fácil y rápida</h4>
+      <p>No pierdas tu tiempo facturando a mano, llevando inventarios y CxC en Excel, Word o sistemas complicados.</p>
     </div>
     </body>  
     </Page>
